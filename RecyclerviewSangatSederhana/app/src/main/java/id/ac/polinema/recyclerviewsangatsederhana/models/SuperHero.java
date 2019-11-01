b06075c6fe225a6227298bf15f0f726efafbac4d
repamungkas <pamungkas.re@gmail.com>
2019-11-01 10:11:44 +0700

    recycle view sangat sederhana

---
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/models/SuperHero.java
@@ -0,0 +1,18 @@
+package id.ac.polinema.recyclerviewsangatsederhana.models;
+
+public class SuperHero {
+    public String heroName;
+
+    public SuperHero(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+}
